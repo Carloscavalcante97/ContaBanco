abe4d708ddae8a0e828b56c1cc101dfa51dc585e
Antônio Carlos Bezerra Cavalcante Junior <126694183+Carloscavalcante97@users.noreply.github.com>
2024-05-29 16:55:07 -0300

    Add files via upload

---
+++ ContaTerminal.java
@@ -0,0 +1,13 @@
+public class ContaTerminal {
+    final String NUMERO = "1021", AGENCIA = "067-8", SENHA = "51030210",NOME ="MARIO ANDRADE";
+    float Saldo = 15375.00F;
+public void Saque(float valorSacado){
+  if(this.Saldo >= valorSacado){
+      this.Saldo -= valorSacado;
+  }else{
+      System.out.println("Saldo insuficiente"+this.Saldo);
+  }
+
+
+}
+}
---
+++ Main.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+     String userConta = "";
+     boolean menu = false;
+        int menuSaque = 0;
+    ContaTerminal userData = new ContaTerminal();
+
+        Scanner read = new Scanner(System.in);
+        System.out.print("Insira sua conta: ");
+        userConta = read.next();
+        System.out.println("Insira sua agência: ");
+        String userAgencia = read.next();
+        System.out.println("Insira sua Senha: ");
+        String userSenha = read.next();
+        while(!userConta.equals(userData.NUMERO) && !userAgencia.equals(userData.AGENCIA) && !userSenha.equals(userData.SENHA)){
+            System.out.print("Insira sua conta Novamente: \n");
+            userConta = read.next();
+            System.out.print("Insira sua agência Novamente: \n");
+            userAgencia = read.next();
+            System.out.print("Insira sua Senha Novamente: \n");
+            userSenha = read.next();
+        }
+        if(userConta.equals(userData.NUMERO) && userAgencia.equals(userData.AGENCIA) && userSenha.equals(userData.SENHA)){
+        System.out.print("Olá "+userData.NOME +", obrigado por criar uma conta em nosso banco,\n" +
+                " sua agência é "+userData.AGENCIA+" , conta "+userData.NUMERO+"\n" +
+                "  seu saldo R$"+userData.Saldo +" já está disponível para saque ");;
+            System.out.print("\n Para realizar um saque digite 1 e para fechar o sitema digite 0? \n");
+            menuSaque = read.nextInt();
+            while(menuSaque != 0 && menuSaque != 1  ){
+                System.out.print("Opção inválida digite novamente: ");
+                menuSaque = read.nextInt();
+            }
+            if (menuSaque == 1){
+                System.out.print("Insira o valor do saque: \n");
+                float valorSacado = read.nextFloat();
+               userData.Saque(valorSacado);
+               System.out.print("Valor sacado com sucesso, Saldo atualizado: R$"+ userData.Saldo + "\nObrigado por usar nosso sistema!");
+
+            }else if(menuSaque == 0){
+                System.out.print("Deslogado com sucesso.");
+            }
+        }
+        }
+        }
+
+
